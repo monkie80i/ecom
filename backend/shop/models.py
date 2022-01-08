7eb20d206e4a09730f4851440321cee727d4b427
Shahzan Sadick <sam.shahzan@gmail.com>
2022-01-08 18:08:23 +0530

    shop models

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
 	name = models.CharField(max_length=100,null=True,blank=True)
-	isActive = models.BooleanField(default=True)
-	isDeleted = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 
@@ -15,7 +16,53 @@ class Product(models.Model):
 	description = models.TextField(null=True,blank=True)
 	price = models.FloatField(null=True,blank=True)
 	stock = models.IntegerField(null=True,blank=True)
-	isActive = models.BooleanField(default=True)
-	isDeleted = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+
+class Wishlist(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+class WishListItems(models.Model):
+	wish_list = models.ForeignKey(Wishlist,related_name='items',on_delete=models.CASCADE,null=True,blank=True)
+	product = models.OneToOneField(Product,on_delete=models.CASCADE,null=True,blank=True)
+	quantity = models.IntegerField(null=True,blank=True)
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+class Cart(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+class CartItems(models.Model):
+	cart = models.ForeignKey(Cart,related_name='items',on_delete=models.CASCADE,null=True,blank=True)
+	product = models.OneToOneField(Product,on_delete=models.CASCADE,null=True,blank=True)
+	quantity = models.IntegerField(null=True,blank=True)
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+class ProductReview(models.Model):
+	user = models.ForeignKey(User,related_name='product_reviews',on_delete=models.DO_NOTHING,null=True,blank=True)
+	product = models.ForeignKey(Product,related_name="reviews",on_delete=models.DO_NOTHING,null=True,blank=True)
+	ratings = models.FloatField()#set cieling value as 5
+	comment = models.TextField()
+	#images
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	
+
+
