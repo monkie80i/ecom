e4f42f5beb717a546ef6e9600bec0bc0ee4d105c
Shahzan Sadick <sam.shahzan@gmail.com>
2022-01-08 15:47:36 +0530

    shop modesl

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=100,null=True,blank=True)
+	isActive = models.BooleanField(default=True)
+	isDeleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+	category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE,null=True,blank=True)
+	name = models.CharField(max_length=100,null=True,blank=True)
+	#images
+	description = models.TextField(null=True,blank=True)
+	price = models.FloatField(null=True,blank=True)
+	stock = models.IntegerField(null=True,blank=True)
+	isActive = models.BooleanField(default=True)
+	isDeleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
