8a9fe84a1d2a4a1db89d335125d359ff6696533f
Shahzan Sadick <sam.shahzan@gmail.com>
2022-01-08 18:39:38 +0530

    order models

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from shop.models import Product
 
 # Create your models here.
+ORDER_STATUS_CHOICES = (
+	('unverified','Unverified'),
+	('verified','Verified'),
+	('dispatched','Dispatched'),
+	('delivered','Delivered')
+)
+
+class Order(models.Model):
+	user = models.ForeignKey(User,related_name='orders',on_delete=models.DO_NOTHING,null=True,blank=True)
+	#delivery_address
+	is_cash_on_delivery = models.BooleanField(default=False,null=True,blank=True) 
+	is_paid = models.BooleanField(default=False,null=True,blank=True)
+	status = models.CharField(max_length=12,choices=ORDER_STATUS_CHOICES,null=True,blank=True)
+	dispatch_date = models.DateTimeField(null=True,blank=True)
+	delivered_date = models.DateTimeField(null=True,blank=True)
+	is_active = models.BooleanField(default=True)
+	is_deleted = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def total_cost(self):
+		pass
+
+class OrderItem(models.Model):
+	order = models.ForeignKey(Order,related_name='items',on_delete=models.DO_NOTHING,null=True,blank=True)
+	product = models.OneToOneField(Product,related_name='order_items',on_delete=models.DO_NOTHING,null=True,blank=True)
+	quantity = models.IntegerField(null=True,blank=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def get_cost(self):
+		return product.price*quantity
