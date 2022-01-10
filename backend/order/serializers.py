fd7c32bc07a22dfacaf5746b6425cdbc12b3cb6a
Shahzan Sadick <sam.shahzan@gmail.com>
2022-01-10 15:34:46 +0530

    order serializer

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,2 +1,32 @@
 from rest_framework import serializers
 from .models import Order,OrderItem
+
+class OrderItemSerilizer(serializers.ModelSerializer):
+	class Meta:
+		model = OrderItem
+		fields = [
+			'id',
+			'order',
+			'product',
+			'quantity',
+			'created'
+		]
+
+class OrderSerializer(serializers.ModelSerializer):
+	#delivery_address = AddressSerializer
+	items = OrderItemSerilizer(many=True)
+
+	class Meta:
+		model = Order
+		fields = [
+			'id',
+			'user',
+			'items'
+			'delivery_address',
+			'is_cash_on_delivery',
+			'is_paid',
+			'status',
+			'dispatch_date',
+			'delivered_date',
+			'created'
+		]
